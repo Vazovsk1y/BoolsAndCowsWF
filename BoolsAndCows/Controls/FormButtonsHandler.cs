2559f487d0e06cc0cd536720b13409d42076c771
Vazovsk1y <anolrey.b@gmail.com>
2023-02-13 21:32:20 +0300

    MVC

--- BoolsAndCows/Controls/FormButtonsHandler.cs
+++ BoolsAndCows/Controls/FormButtonsHandler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using BoolsAndCows.Controls.Buttons;
-using BoolsAndCows;
 using BoolsAndCows.Models;
 
 namespace BoolsAndCows.Controls
